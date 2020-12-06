74134128eccc269ae23e9ef88d36ea145514ef9a
sahm124 <saham@users.noreply.github.com>
2020-12-06 15:11:20 +0330

    MainPageAdmin updated

--- src/main/java/Plato/Model/Event.java
+++ src/main/java/Plato/Model/Event.java
@@ -30,6 +30,78 @@ public class Event {
 
     }
 
+    public static ArrayList<Event> getEvents() {
+        return events;
+    }
+
+    public static void setEvents(ArrayList<Event> events) {
+        Event.events = events;
+    }
+
+    public ArrayList<Player> getPlayersOfThisEvent() {
+        return playersOfThisEvent;
+    }
+
+    public void setPlayersOfThisEvent(ArrayList<Player> playersOfThisEvent) {
+        this.playersOfThisEvent = playersOfThisEvent;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getEventScore() {
+        return eventScore;
+    }
+
+    public void setEventScore(int eventScore) {
+        this.eventScore = eventScore;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    public boolean isHasStarted() {
+        return hasStarted;
+    }
+
+    public void setHasStarted(boolean hasStarted) {
+        this.hasStarted = hasStarted;
+    }
+
+    public boolean isHasEnded() {
+        return hasEnded;
+    }
+
+    public void setHasEnded(boolean hasEnded) {
+        this.hasEnded = hasEnded;
+    }
+
     public void startEvent(int eventId){
 
     }
