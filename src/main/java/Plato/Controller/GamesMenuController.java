e82dbfc02df093bafa785d2a2752c06b26038c85
sinanikdel <69875074+sinanikdel@users.noreply.github.com>
2020-12-07 10:07:05 +0330

    view : GamesMenu controller : GamesMenuController

--- src/main/java/Plato/Controller/GamesMenuController.java
+++ src/main/java/Plato/Controller/GamesMenuController.java
@@ -1,10 +1,13 @@
 package Plato.Controller;
 
 import Plato.Model.Game;
+import Plato.Model.Player;
+import Plato.View.GamesMenu;
 
 import java.util.ArrayList;
 
-public class GamesMenuController {
+public class GamesMenuController  {
+    private Player player;
     private static GamesMenuController gamesMenuController = new GamesMenuController();
 
     public static GamesMenuController getInstance() {
@@ -15,4 +18,12 @@ public class GamesMenuController {
         ArrayList<Game> game = Game.getGames();
         return null;
     }
+    private void setGamesMenuController (Player player){
+
+    }
+    private String getGames (){
+        return null;
+    }
+
+
 }
--- src/main/java/Plato/View/GamesMenu.java
+++ src/main/java/Plato/View/GamesMenu.java
@@ -1,12 +1,21 @@
 package Plato.View;
 
 import Plato.Controller.GamesMenuController;
+import Plato.Model.Player;
 
 public class GamesMenu extends Page{
+    private Player player;
     private static GamesMenu gamesMenu = new GamesMenu();
     private GamesMenuController gamesMenuController = GamesMenuController.getInstance();
     public Page run() {
         gamesMenuController.getGamesController();
         return null;
     }
+    private void setPlayer(Player player){
+
+    }
+    public Page openGame (String name){
+        return null;
+    }
+
 }
