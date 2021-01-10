05c09405f3e5a2b35e1cc46455d430162524ea4a
sahm124 <saham@users.noreply.github.com>
2021-01-10 20:10:34 +0330

    graphical first11

---
+++ src/module-info.java
@@ -0,0 +1,5 @@
+module graphical {
+    requires javafx.fxml;
+    requires javafx.controls;
+    opens sample;
+}
---
+++ src/sample/Controller.java
@@ -0,0 +1,58 @@
+package sample;
+
+import Plato.Controller.LoginPageController;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    private static Label message ;
+    @FXML
+    private static TextField fusername ;
+    @FXML
+    private static TextField femail ;
+    @FXML
+    private static TextField ffname ;
+    @FXML
+    private static TextField flname;
+    @FXML
+    private static TextField fpnumber ;
+    @FXML
+    private static PasswordField fpassword;
+
+    public static void register()
+    {
+        boolean result = LoginPageController.loginPageController.register(fusername.getText(),fpassword.getText(),femail.getText(),ffname.getText(),flname.getText(),fpnumber.getText());
+        if (!result)
+        {
+            message.setText("username is used");
+            message.setVisible(true);
+
+        }
+        else {
+            message.setText("you are succesfully registered");
+            message.setVisible(true);
+        }
+
+    }
+    public static void login()
+    {
+        String result = LoginPageController.loginPageController.loginController(fusername.getText(),fpassword.getText());
+        if (result.equalsIgnoreCase("invalid information"))
+        {
+            message.setText("invalid information");
+            message.setVisible(true);
+
+        }
+        else if (result.equalsIgnoreCase("found admin")){
+
+        }
+        else if (result.equalsIgnoreCase("found player"))
+        {
+
+        }
+    }
+}
