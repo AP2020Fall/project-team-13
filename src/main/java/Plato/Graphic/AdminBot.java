bf8b05ed3cd9619e62311f4dd6d209ff6af385c4
sahm124 <saham@users.noreply.github.com>
2021-01-16 17:16:10 +0330

    plato graphic some classes created3

--- src/main/java/Plato/Graphic/AdminBot.java
+++ src/main/java/Plato/Graphic/AdminBot.java
@@ -1,11 +1,38 @@
 package Plato.Graphic;
 
+import Plato.Controller.LoginPageController;
+import Plato.Controller.MainPageAdminController;
+import Plato.Model.Admin;
+import Plato.Model.Player;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
-import javax.swing.text.TableView;
-import java.awt.*;
+import javax.swing.text.Element;
+import javax.swing.text.TabableView;
+import javax.swing.text.View;
+
 
 public class AdminBot {
+    private MainPageAdminController mainPageAdminController = MainPageAdminController.getInstance();
+
     @FXML
-    static TextArea botNewMessage = new TextArea();
+     TextField botNewMessage = new TextField();
+    @FXML
+    TableView<String> tableOfBotMessages = new TableView<String>();
+
+    @FXML
+    public void createNewMessage()
+    {
+        mainPageAdminController.addbotMessage(botNewMessage.getText());
+    }
+    @FXML
+    public void showBotMessages()
+    {
+
+        for (String message : Player.getPlayers().get(1).getMessages()) {
+
+        }
+    }
+
 }
