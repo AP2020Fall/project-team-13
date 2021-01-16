9260e8b9cef93984893fcd875e670965db03f1a1
sahm124 <saham@users.noreply.github.com>
2021-01-16 19:32:46 +0330

    plato graphic some classes updated2

--- src/main/java/Plato/Graphic/PlayerBot.java
+++ src/main/java/Plato/Graphic/PlayerBot.java
@@ -1,4 +1,25 @@
 package Plato.Graphic;
 
+import Plato.Controller.LoginPageController;
+import Plato.Model.Player;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+import javax.swing.text.TabableView;
+import java.awt.event.ActionEvent;
+
 public class PlayerBot {
+    @FXML
+    TableView<String> botMessage = new TableView<>();
+
+    public void showmes(javafx.event.ActionEvent event) {
+        Player player = (Player) LoginPageController.user;
+        ObservableList mes = FXCollections.observableArrayList();
+        for (String message : player.getMessages()) {
+            mes.add(message);
+        }
+        botMessage.setItems(mes);
+    }
 }
