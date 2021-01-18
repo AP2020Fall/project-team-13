e4bb41acc1f8dacbec251a4391cb220490c741f1
sinanikdel <69875074+sinanikdel@users.noreply.github.com>
2021-01-18 17:08:01 +0330

    Friends class 50%

--- src/main/java/Plato/Graphic/Friends.java
+++ src/main/java/Plato/Graphic/Friends.java
@@ -3,8 +3,13 @@ package Plato.Graphic;
 import Plato.MainGraphical;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class Friends {
+
+    public javafx.scene.control.TextArea TextArea;
+    public TextField username;
 
     @FXML
     public void joinevent(javafx.event.ActionEvent event)
@@ -47,5 +52,23 @@ public class Friends {
     public void Logout(ActionEvent event)
     {
         MainGraphical.scene.setRoot(MainGraphical.root);
+    }
+
+    public void accept(ActionEvent actionEvent) {
+    }
+
+    public void decline(ActionEvent actionEvent) {
+    }
+
+    public void search(ActionEvent actionEvent) {
+    }
+
+    public void addFriend(ActionEvent actionEvent) {
+    }
+
+    public void removeFriend(ActionEvent actionEvent) {
+    }
+
+    public void showRequests(ActionEvent actionEvent) {
     }
 }
