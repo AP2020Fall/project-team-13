603638a540fbce78915728189290bdc40c5e0362
sinanikdel <69875074+sinanikdel@users.noreply.github.com>
2021-01-18 17:01:07 +0330

    ManageEvents class 50%

--- src/main/java/Plato/Graphic/ManageEvents.java
+++ src/main/java/Plato/Graphic/ManageEvents.java
@@ -1,8 +1,17 @@
 package Plato.Graphic;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class ManageEvents {
 
 
+    public TextField gameName;
+    public TextField endDate;
+    public TextField score;
+    public TextField startDate;
+
+    public void addEvent(ActionEvent actionEvent) {
+    }
 }
